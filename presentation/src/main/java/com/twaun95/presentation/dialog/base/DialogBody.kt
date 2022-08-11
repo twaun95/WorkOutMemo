84150d9bfb51e1e7c2e213faf19a3400f353527e
taewaun <taewaunkwon@gmail.com>
2022-08-11 21:25:23 +0900

    공통 Dialog

---
+++ presentation/src/main/java/com/twaun95/presentation/dialog/base/DialogBody.kt
@@ -0,0 +1,6 @@
+package com.twaun95.presentation.dialog.base
+
+data class DialogBody(
+    val title: String,
+    val message: String? = null
+)
--- presentation/src/main/java/com/twaun95/presentation/ui/body_part/BodyPartFragment.kt
+++ presentation/src/main/java/com/twaun95/presentation/ui/body_part/BodyPartFragment.kt
@@ -4,10 +4,11 @@ import com.twaun95.core.BaseFragment
 import com.twaun95.data.TestRepository
 import com.twaun95.presentation.R
 import com.twaun95.presentation.databinding.FragmentBodyPartBinding
+import com.twaun95.presentation.dialog.base.BaseDialog
+import com.twaun95.presentation.dialog.CommonDialog
+import com.twaun95.presentation.dialog.base.DialogBody
 import com.twaun95.presentation.extensions.setOnSingleClickListener
 import com.twaun95.presentation.ui.body_part.diff_basic.BodyPartAdapter
-import com.twaun95.presentation.dialog.CreateBodyPartDialog
-import com.twaun95.presentation.dialog.DialogClicked
 import com.twaun95.presentation.ui.memo.list.MemoListFragment
 import com.twaun95.presentation.utils.MyLogger
 import org.koin.android.ext.android.inject
@@ -43,16 +44,27 @@ class BodyPartFragment: BaseFragment<FragmentBodyPartBinding, BodyPartFragmentVi
     override fun setEvent() {
         super.setEvent()
         binding.btnCreate.setOnSingleClickListener {
-            CreateBodyPartDialog.show(childFragmentManager) {
-                when (it) {
-                    DialogClicked.NO -> {
-                        MyLogger.d("No")
-                    }
-                    DialogClicked.YES -> {
-                        MyLogger.d("Yes")
-                    }
+            CommonDialog(
+                BaseDialog.ButtonType.TWO,
+                DialogBody("제목", "내용"),
+                {
+                    MyLogger.d("No")
+                }, {
+                    MyLogger.d("Yes")
                 }
-            }
+            ).show(childFragmentManager, null)
+
+
+//            CreateBodyPartDialog.show(childFragmentManager) {
+//                when (it) {
+//                    DialogClicked.NO -> {
+//                        MyLogger.d("No")
+//                    }
+//                    DialogClicked.YES -> {
+//                        MyLogger.d("Yes")
+//                    }
+//                }
+//            }
         }
     }
 
