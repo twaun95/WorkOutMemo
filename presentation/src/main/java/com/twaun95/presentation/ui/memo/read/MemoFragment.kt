5fe05396e1a9b785a78048e1c5e262ee02490141
taewaun <taewaunkwon@gmail.com>
2022-08-14 23:56:25 +0900

    memo create, read view setting

--- presentation/src/main/java/com/twaun95/presentation/ui/memo/read/MemoFragment.kt
+++ presentation/src/main/java/com/twaun95/presentation/ui/memo/read/MemoFragment.kt
@@ -3,6 +3,7 @@ package com.twaun95.presentation.ui.memo.read
 import com.twaun95.core.BaseFragment
 import com.twaun95.presentation.R
 import com.twaun95.presentation.databinding.FragmentMemoBinding
+import com.twaun95.presentation.extensions.setOnSingleClickListener
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class MemoFragment : BaseFragment<FragmentMemoBinding, MemoFragmentViewModel>(R.layout.fragment_memo) {
@@ -10,6 +11,26 @@ class MemoFragment : BaseFragment<FragmentMemoBinding, MemoFragmentViewModel>(R.
 
     // 운동 종목 입력
     // 내용
+
+    override fun initView() {
+        super.initView()
+    }
+
+    override fun setEvent() {
+        super.setEvent()
+
+        binding.btnBack.setOnSingleClickListener {
+            // 메모 취소 완료? 다이얼로그
+            parentFragmentManager.beginTransaction()
+                .setCustomAnimations(R.anim.slide_enter_from_left, R.anim.slide_exit_to_right)
+                .remove(this)
+                .commit()
+        }
+
+        binding.btnUpdate.setOnSingleClickListener {
+            // 메모 저장
+        }
+    }
 
     companion object {
         fun newInstance() = MemoFragment()
